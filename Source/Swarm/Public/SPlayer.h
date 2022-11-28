1aef265ed33e5d5518425ab552a5ba20c4eb485c
Felipe Libardi <flibardig@gmail.com>
2022-11-28 20:37:50 -0300

    Player class inheriting from character added.

---
+++ Source/Swarm/Public/SPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SCharacter.h"
+#include "SPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWARM_API ASPlayer : public ASCharacter
+{
+	GENERATED_BODY()
+	
+};
