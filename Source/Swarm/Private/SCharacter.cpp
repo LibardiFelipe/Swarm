e2ae63deeaf4aa584ff4ff75d543bf59459065de
Felipe Libardi <flibardig@gmail.com>
2022-11-28 23:36:41 -0300

    Partial damage handling has been implemented.

--- Source/Swarm/Private/SCharacter.cpp
+++ Source/Swarm/Private/SCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "SCharacter.h"
+#include "SPlayerController.h"
+#include "Net/UnrealNetwork.h"
+#include "SGameMode.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -9,6 +12,18 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Health = HUMAN_MAX_HEALTH;
+}
+
+float ASCharacter::GetMaxHealth() const
+{
+	switch (PlayerClass) {
+		case EPlayerClass::ZOMBIE: return ZOMBIE_MAX_HEALTH;
+		case EPlayerClass::NEMESIS: return NEMESIS_MAX_HEALTH;
+		case EPlayerClass::SURVIVOR: return SURVIVOR_MAX_HEALTH;
+		default:
+			return HUMAN_MAX_HEALTH;
+	}
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +31,9 @@ void ASCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (HasAuthority()) {
+		GameMode = Cast<ASGameMode>(GetWorld()->GetAuthGameMode());
+	}
 }
 
 // Called every frame
@@ -30,5 +48,68 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void ASCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ASCharacter, Health);
+	DOREPLIFETIME(ASCharacter, PlayerClass);
+	DOREPLIFETIME(ASCharacter, bRageMode);
+	DOREPLIFETIME(ASCharacter, bHasInfectionShield);
+	DOREPLIFETIME_CONDITION(ASCharacter, PlayerController, COND_OwnerOnly);
+}
+
+float ASCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	// No need to beat the corpse
+	if (!IsPlayerAlive())
+		return 0.f;
+
+	auto instigatorCharacter = Cast<ASCharacter>(EventInstigator->GetCharacter());
+	if (!IsValid(instigatorCharacter) || instigatorCharacter->IsPlayerHuman() == IsPlayerHuman())
+		return 0.f;
+
+	if (GameMode->ShouldHumansBeInfected()) {
+		if (bHasInfectionShield) {
+			ChangeInfectionShield(false);
+			return 0.f;
+		}
+
+		// TODO: Call infect player function
+
+		return 99999.f;
+	}
+	
+	auto trueDamage = bRageMode ? ((RageModeDamageReduction / 100.f) * DamageAmount) : DamageAmount;
+	Health = FMath::Clamp(Health - trueDamage, -5.f, GetMaxHealth());
+
+	if (!IsPlayerAlive()) {
+		// TODO: Call kill player functiom
+	}
+
+	return trueDamage;
+}
+
+void ASCharacter::ChangeInfectionShield(bool bActivate)
+{
+	// TODO: Call a MultiCast event with sound and particles
+
+	bHasInfectionShield = bActivate;
+}
+
+void ASCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	PlayerController = Cast<ASPlayerController>(NewController);
+}
+
+void ASCharacter::UnPossessed()
+{
+	Super::UnPossessed();
+
+	PlayerController = nullptr;
 }
 
--- Source/Swarm/Public/SCharacter.h
+++ Source/Swarm/Public/SCharacter.h
@@ -4,7 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Enums.h"
+
 #include "SCharacter.generated.h"
+
+class ASPlayerController;
+class ASGameMode;
 
 UCLASS()
 class SWARM_API ASCharacter : public ACharacter
@@ -12,18 +17,64 @@ class SWARM_API ASCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ASCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser);
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void UnPossessed() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Stats")
+		float GetHealth() const { return Health; }
+	
+	UFUNCTION(BlueprintCallable, Category = "Stats")
+		bool IsPlayerAlive() const { return Health > 0.f; }
+
+	UFUNCTION(BlueprintCallable, Category = "Stats")
+		float GetMaxHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Stats")
+		bool IsPlayerHuman() const { return PlayerClass == EPlayerClass::HUMAN || PlayerClass == EPlayerClass::SURVIVOR; };
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Replicated, Category = "Stats")
+		ASPlayerController* PlayerController;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "World")
+		ASGameMode* GameMode;
 
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Replicated, Transient, Category = "Stats")
+		float Health{ 0.f };
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Stats")
+		float HUMAN_MAX_HEALTH{ 250.f };
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Stats")
+		float ZOMBIE_MAX_HEALTH{ 3200.f };
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Stats")
+		float NEMESIS_MAX_HEALTH{ 12000.f };
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Stats")
+		float SURVIVOR_MAX_HEALTH{ 12000.f };
+
+	// TODO: Turn into a replicatedUsing function with visual effects
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Replicated, Category = "Stats")
+		bool bRageMode{ false };
+
+	// TODO: Turn into a replicatedUsing function with visual effects
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Replicated, Category = "Stats")
+		bool bHasInfectionShield{ false };
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Stats", meta = (ClampMin = "0.0", ClampMax = "100.0", UIMin = "0.0", UIMax = "100.0"))
+		float RageModeDamageReduction{ 50.f };
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Replicated, Category = "Stats")
+		EPlayerClass PlayerClass{ EPlayerClass::HUMAN };
+
+	void ChangeInfectionShield(bool bActivate);
 };
