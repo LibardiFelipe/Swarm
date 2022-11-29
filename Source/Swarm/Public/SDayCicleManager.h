1170c34118ab6c39b622131691101c48c1e6594d
Felipe Libardi <flibardig@gmail.com>
2022-11-29 19:13:08 -0300

    Day/night replicated cicle added.

---
+++ Source/Swarm/Public/SDayCicleManager.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SDayCicleManager.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnDayTimeSwitch, bool, bIsDay);
+
+class UDirectionalLightComponent;
+class URotatingMovementComponent;
+class UArrowComponent;
+
+UCLASS()
+class SWARM_API ASDayCicleManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASDayCicleManager();
+	virtual void Tick(float DeltaTime) override;
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintAssignable, Category = "Gameplay")
+		FOnDayTimeSwitch OnDayTimeSwitch;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings")
+		float DayCicleInMinutes{ 10.f };
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings")
+		bool bInvertRotation{ false };
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings")
+		float SunRotationReplicationInterval{ .1f };
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+		UDirectionalLightComponent* DirectionalLight;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+		URotatingMovementComponent* RotatingMovement;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+		UArrowComponent* ArrowComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, ReplicatedUsing = OnRep_ActorRotation, Category = "Settings")
+		FRotator ActorRotation = FRotator::ZeroRotator;
+
+	UFUNCTION()
+		void OnRep_ActorRotation();
+	UFUNCTION()
+		void ReplicateActorRotation();
+};
